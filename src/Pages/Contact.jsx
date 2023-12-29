65612159315d6eda5c73021f5e86d73c441b80f4
Aniruddhsinh Jadeja <anirudhdhsinhj@gmail.com>
2023-12-29 17:17:15 -0500

    Integrates Confirmation Dialog
    New Files:
    	in src/Components/ConfirmationDialog/:
    		+ ConfirmationDialog.css
    Changed Files:
    	in src/Components/ConfirmationDialog/ConfirmationDialog.jsx:
    		+ Adds component closing functionality:
    
    	in src/Pages/Contact.jsx:
    		M Optimizes the code
    		+ implements the confimation dialog code
    Deleted Files:
    Fixes:
    Future Work:

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,26 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { availableWidth, getAvailableWidth } from '..';
+import axios from 'axios';
+import { Puff } from 'react-loading-icons';
+import { getAvailableWidth, getAvailableHeight } from '..';
+import { getCurrentPageData, sendMessage } from '../firebase/manageRealtimeDatabase.mjs';
+import UserMessage from '../models/UserMessage';
 import DottedDiv from '../Components/DottedDiv/DottedDiv';
-import ButtonPrimary from '../Components/ButtonPrimary/ButtonPrimary';
 import AnimatedPage from '../Components/Animated/AnimatedPage';
 import Footer from '../Components/Footer/Footer';
-import { getCurrentPageData, sendMessage } from '../firebase/manageRealtimeDatabase.mjs';
 import InputText from '../Components/InputText/InputText';
-import axios from 'axios';
-import UserMessage from '../models/UserMessage';
 import arrow from '../assets/arrow.gif';
-import { Puff } from 'react-loading-icons'
-import { set } from 'firebase/database';
-
+import ConfirmationDialog from '../Components/ConfirmationDialog/ConfirmationDialog';
 
 const Contact = () => {
-  const [sectionHeight, setSectionHeight] = useState('auto'); // Initialize section height as 'auto'
-  const [articleHeight, setArticleHeight] = useState('auto'); // Initialize article height as 'auto'
-  const [subArticleHeight, setSubArticleHeight] = useState('auto'); // Initialize sub article height as 'auto'
-  const [dottedDivisionWidth, setDottedDivisionWidth] = useState('270'); // Initialize isMobileViewActive as true
-  const [dottedDivisionHeight, setDottedDivisionHeight] = useState(130); // Initialize isMobileViewActive as true
-  const [resumeFileUrl, setResumeFileUrl] = useState(''); // Initialize isMobileViewActive as true
+  const [sectionHeight, setSectionHeight] = useState('auto'); 
+  const [articleHeight, setArticleHeight] = useState('auto');
+  const [subArticleHeight, setSubArticleHeight] = useState('auto');
+  const [dottedDivisionWidth, setDottedDivisionWidth] = useState('270');
+  const [dottedDivisionHeight, setDottedDivisionHeight] = useState(130);
+  const [resumeFileUrl, setResumeFileUrl] = useState('');
   const [resumePicture, setResumePicture] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -33,21 +31,20 @@ const Contact = () => {
   const [isMessageError, setIsMessageError] = useState(false);
   const [isMessageErrorMessage, setIsMessageErrorMessage] = useState('');
 
+
   const imageHeight = useRef(560);
+  const availableWidth = useRef(getAvailableWidth());
+  const availableHeight = useRef(getAvailableHeight());
+  const isMessageSent = useRef(false);
+  const isError = useRef(false);
   const navigate = useNavigate();
+  const handleInputChange = (setValue, value) => setValue(value);
 
+  const [isDialogVisible, setDialogVisible] = useState(false);
 
-  const handleEmailChange = (value) => {
-    setEmail(value);
-  }
-
-  const handleNameChange = (value) => {
-    setName(value);
-  }
-
-  const handleMesssageChange = (value) => {
-    setMessage(value);
-  }
+  const openDialog = () => {
+    setDialogVisible(true);
+  };
 
   const handleMessageSend = () => {
     if (validateInputs()) {
@@ -55,191 +52,143 @@ const Contact = () => {
       setIsEmailError(false);
       setIsMessageError(false);
       setIsLoading(true);
-      const userM = {
-        name: name,
-        email: email,
-        message: message
-      }
-      const userMessage = new UserMessage(userM)
-      sendMessage(userMessage).then((result) => {
-        setIsLoading(false);
-      }).catch((error) => {
-        console.log(error);
-        setIsLoading(false);
-      });
-    }
-    else {
+
+      const userMessage = new UserMessage({ name, email, message });
+
+      sendMessage(userMessage)
+        .then(() => { 
+          setIsLoading(false) 
+          isMessageSent.current = true;
+          isError.current = false;
+          openDialog();
+        })
+        .catch((error) => {
+          console.log(error);
+          setIsLoading(false);
+          isError.current = true;
+          openDialog();
+        });
+    } else {
       setIsLoading(false);
     }
-  }
+  };
 
-  function validateEmail(email) {
-    var re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    let res = re.test(String(email).toLowerCase());
-    if (res) {
+  const validateEmail = (email) => {
+    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const isValid = re.test(String(email).toLowerCase());
+
+    if (isValid) {
       setIsEmailError(false);
       setIsEmailErrorMessage(' ');
       return true;
-    }
-    else {
+    } else {
       setIsEmailError(true);
       setIsEmailErrorMessage('Please enter a valid email address');
       return false;
     }
-  }
+  };
 
-  function validateName(name) {
-    if (name !== '' && name.length > 2 && name.length < 50 &&
-    name !== undefined && name !== null && name !== false && name !== true
-    && name !== NaN && name !== 0 && name !== '0' && name !== 'false' && name !== 'null' && name !== 'undefined' && name !== 'NaN' && name !== 'true') {
+  const validateName = (name) => {
+    const isValid =
+      name !== '' &&
+      name.length > 2 &&
+      name.length < 50 ;
+    if (isValid) {
       setIsNameError(false);
       setNameErrorMessage(' ');
       return true;
-    }
-    else {
+    } else {
       setNameErrorMessage('Name must be between 3 and 50 characters');
       setIsNameError(true);
       return false;
     }
-  }
+  };
 
-  function validateMessage(message) {
-    if (message.length > 10 && message !== '' && message !== undefined && message !== null && message !== false && message !== true && message !== NaN && message !== 0 && message !== '0' && message !== 'false' && message !== 'null' && message !== 'undefined' && message !== 'NaN' && message !== 'true') {
+  const validateMessage = (message) => {
+    const isValid =
+      message.length > 10;
+
+    if (isValid) {
       setIsMessageError(false);
       setIsMessageErrorMessage(' ');
       return true;
-    }
-    else {
+    } else {
       setIsMessageError(true);
-      setIsMessageErrorMessage('Message must be atleast 10 characters long');
+      setIsMessageErrorMessage('Message must be at least 10 characters long');
       return false;
     }
-  }
+  };
 
-  function validateInputs() {
-
-    if (validateName(name) && validateEmail(email) && validateMessage(message)) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-
-  const avialableWidth = useRef(getAvailableWidth());
-  //console.log("Available width " + avialableWidth.current)
-  const avialableHeight = useRef(window.innerHeight);
+  const validateInputs = () =>
+    validateName(name) && validateEmail(email) && validateMessage(message);
 
   const calculateHeights = () => {
-    const navBar = document.getElementById('middleNavigationBar'); //  Navigationbar's ID
+    const navBar = document.getElementById('middleNavigationBar');
     const footer = document.getElementById('footer');
     const contactForm = document.getElementById('contactForm');
 
-
     if (navBar && footer && contactForm) {
-      // Calculate section height by subtracting Navigationbar and footer heights
       const newSectionHeight = `calc(100vh - 194px)`;
-
-      // Calculate article height by subtracting footer height
       const newArticleHeight = `calc(${newSectionHeight} - 64px)`;
-
       const newSubArticleHeight = `calc(${newSectionHeight} - 96px)`;
-      // console.log("Available width " + availableWidth.current )
 
-      setDottedDivisionWidth(contactForm.offsetWidth)
+      setDottedDivisionWidth(contactForm.offsetWidth);
 
-      if (avialableWidth.current < 359) {
+      if (availableWidth.current < 359) {
         setDottedDivisionHeight(100);
-      }
-      else if (availableWidth.current < 560) {
-        imageHeight.current = ((avialableHeight.current * 60) / 100)
-      }
-      else if (avialableWidth.current < 768) {
-        imageHeight.current = ((avialableHeight.current * 50) / 100)
-      }
-      else if (avialableWidth.current < 1024) {
-        imageHeight.current = ((avialableHeight.current * 60) / 100)
-      }
-      else {
-        imageHeight.current = ((avialableHeight.current * 70) / 100)
+      } else if (availableWidth.current < 560) {
+        imageHeight.current = (availableHeight.current * 60) / 100;
+      } else if (availableWidth.current < 768) {
+        imageHeight.current = (availableHeight.current * 50) / 100;
+      } else if (availableWidth.current < 1024) {
+        imageHeight.current = (availableHeight.current * 60) / 100;
+      } else {
+        imageHeight.current = (availableHeight.current * 70) / 100;
       }
 
-      // Set the calculated heights
       setSectionHeight(newSectionHeight);
       setArticleHeight(newArticleHeight);
       setSubArticleHeight(newSubArticleHeight);
-
     }
-
   };
-
 
   useEffect(() => {
     calculateHeights();
-    getCurrentPageData().then((data) => {
-      setResumePicture(data.resumePictureUrl);
-      setResumeFileUrl(data.resumeFileUrl);
-    }).catch((error) => {
-      navigate('/error');
-    });
-
+    getCurrentPageData()
+      .then((data) => {
+        setResumePicture(data.resumePictureUrl);
+        setResumeFileUrl(data.resumeFileUrl);
+      })
+      .catch(() => navigate('/error'));
   }, []);
 
-
-
   const handleDownload = async () => {
-    // This can be downloaded directly:
     try {
-      // Make an HTTP request to get the file as a blob
       const response = await axios.get(resumeFileUrl, {
         responseType: 'blob',
       });
-
-      // Create a blob from the response data
       const blob = new Blob([response.data], { type: 'application/pdf' });
-
-      // Create a link element
       const link = document.createElement('a');
-
-      // Set the href attribute with the blob URL
       link.href = window.URL.createObjectURL(blob);
-
-      // Set the download attribute with the desired filename
       link.download = 'Resume_Anirudhdhsinh_Jadeja.pdf';
-
-      // Append the link to the document
       document.body.appendChild(link);
-
-      // Trigger a click on the link to initiate the download
       link.click();
-
-      // Remove the link from the document
       document.body.removeChild(link);
     } catch (error) {
       console.error('Error downloading file:', error);
     }
-    // window.open(resumeFileUrl);
   };
 
   return (
     <AnimatedPage>
       <section id='homePage' style={{ minHeight: sectionHeight }}>
-        <p className='w-2 fixed lg:hidden bottom-32 right-3.5 opacity-60 text-center text-xs text-[var(--color-primary-white)]'> C o n t a c t</p>
-        {/* Article */}
-        <article
-          id='article'
-          className='pt-14'
-          style={{ minHeight: articleHeight }}>
-          {/* Left and Right Article Divs */}
+        <p className='w-2 fixed lg:hidden bottom-32 right-3.5 opacity-60 text-center text-xs text-[var(--color-primary-white)]'>
+          Contact
+        </p>
+        <article id='article' className='pt-14' style={{ minHeight: articleHeight }}>
           <div className={'mx-5 grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-[60%_40%] gap-4'}>
-
-
             <div className={'py-10 align-center lg:my-auto flex-col'} id='leftArticle' style={{ height: availableWidth < 768 ? imageHeight.current / 1.4 : imageHeight.current, position: 'relative' }}>
-              <img className='' src={resumePicture} style={{
-                objectFit: 'cover', objectPosition: 'center top',
-                width: availableWidth < 768 ? '100%' : '90%', maxHeight: availableWidth < 768 ? imageHeight.current / 1.4 : imageHeight.current
-              }} />
-
+              <img className='' src={resumePicture} style={{ objectFit: 'cover', objectPosition: 'center top', width: availableWidth < 768 ? '100%' : '90%', maxHeight: availableWidth < 768 ? imageHeight.current / 1.4 : imageHeight.current }} />
               <button
                 className='absolute bottom-[12%] lg:bottom-[22%] mx-auto my-auto transform bg-[var(--color-primary-accent)] text-white rounded-md px-4 py-2 mt-4 hover:bg-[var(--color-primary-accent-hover)] transition-all duration-300 ease-in-out'
                 onClick={handleDownload}
@@ -247,41 +196,31 @@ const Contact = () => {
                 Download Resume
               </button>
             </div>
-
-
-
-
-            <div className={(avialableWidth.current < 1024) ?
-              // Mobile CSS 
+            <div className={(availableWidth.current < 1024) ?
               'text-[var(--color-primary-white)] p-2 mt-8 mb-8'
-              : // Desktop CSS 
-              ` text-[var(--color-primary-white)] p-2 align-center w-[95%]`
-            }
+              : ` text-[var(--color-primary-white)] p-2 align-center w-[95%]`}
               id='rightArticle'
-
-              style={{ height: (avialableWidth.current < 1024) ? "fit-content" : subArticleHeight, minHeight: (avialableWidth.current < 1024) ? "fit-content" : subArticleHeight }}>
-
-
+              style={{ height: (availableWidth.current < 1024) ? "fit-content" : subArticleHeight, minHeight: (availableWidth.current < 1024) ? "fit-content" : subArticleHeight }}>
               <div id='contactForm' className=' w-full'>
                 <div className='grid lg:grid-cols-2 gap-2'>
-                  <InputText placeholder="Name" onInputChange={handleNameChange} isError={isNameError} errorMessage={NameErrorMessage} validator={validateName} />
-                  <InputText placeholder="Email" onInputChange={handleEmailChange} isError={isEmailError} errorMessage={isEmailErrorMessage} validator={validateEmail} />
+                  <InputText placeholder="Name" onInputChange={(value) => handleInputChange(setName, value)} isError={isNameError} errorMessage={NameErrorMessage} validator={validateName} />
+                  <InputText placeholder="Email" onInputChange={(value) => handleInputChange(setEmail, value)} isError={isEmailError} errorMessage={isEmailErrorMessage} validator={validateEmail} />
                 </div>
-                <InputText placeholder="Message"
-                  onInputChange={handleMesssageChange} isError={isMessageError} errorMessage={isMessageErrorMessage} validator={validateMessage}/>
+                <InputText placeholder="Message" onInputChange={(value) => handleInputChange(setMessage, value)} isError={isMessageError} errorMessage={isMessageErrorMessage} validator={validateMessage} />
                 <DottedDiv className="align-center" height={dottedDivisionHeight} width={dottedDivisionWidth} />
                 <div className='sm:-mt-4 sm2:mt-0'>
-
                   <button
-                    className={
-                      'bg-var(--color-primary-black) h-auto w-auto px-4 align-center mt-5 font-inter border-[1px] border-solid border-[var(--color-primary-accent)] flex p-1'
-                    }
+                    className={'bg-var(--color-primary-black) h-auto w-auto px-4 align-center mt-5 font-inter border-[1px] border-solid border-[var(--color-primary-accent)] flex p-1'}
                     onClick={handleMessageSend}
-                  > {isLoading ? (<Puff />) :
-                    (<div className='flex'>
-                      <p className='mt-1'>Send Message</p>
-                      <img src={arrow} height={30} width={30} className="-rotate-90 mt-1 ml-2" />
-                    </div>)}
+                  >
+                    {isLoading ? (
+                      <Puff />
+                    ) : (
+                      <div className='flex'>
+                        <p className='mt-1'>Send Message</p>
+                        <img src={arrow} height={30} width={30} className="-rotate-90 mt-1 ml-2" />
+                      </div>
+                    )}
                   </button>
                 </div>
               </div>
@@ -290,14 +229,11 @@ const Contact = () => {
         </article>
         <div className='max-w-[var(--max-width)] mx-auto  w-full mt-12'>
           <Footer home={false} />
+          <ConfirmationDialog isError={false} isVisible={isDialogVisible} setDialogVisible={setDialogVisible} />
         </div>
       </section>
     </AnimatedPage>
   );
 };
-
-
-
-
 
 export default Contact;
