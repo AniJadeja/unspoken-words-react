8952c2cd610019a2a70c9d8f5c8ada4f2b21ba22
Parthgujjar2060 <parthgujjar2003@gmail.com>
2024-01-07 01:05:21 -0500

    Github links fixed in each row

--- src/models/ProjectModel.js
+++ src/models/ProjectModel.js
@@ -3,11 +3,11 @@ class ProjectModel {
       this.name = data.name;
       this.description = data.description;
       this.images = data.images;
-      this.technologies = data.technologies;
+      this.technologies = data.technologies; 
       this.features = data.features;
       this.isDarken = data.isDarken;
+      this.github = data.github;
     }
   }
-
 
 export default ProjectModel;
